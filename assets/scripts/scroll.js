1fe5849841b445060c887d5f023e7d86fac91609
cliftondavies <clifton.davies@outlook.com>
2020-02-14 17:16:18 +0000

    Fix issues in hidden Nav Bar

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -3,7 +3,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
     document.getElementById("first-navbar").style.top = "0";
+    document.getElementById("nav-hide").style.display = "block";
   } else {
     document.getElementById("first-navbar").style.top = "-60px";
+    document.getElementById("nav-hide").style.display = "none";
   }
 }
