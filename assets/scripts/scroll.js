1311354e7708d7c015e54dbcacad5c6af284085f
cliftondavies <clifton.davies@outlook.com>
2020-02-15 01:30:56 +0000

    Fix interactivity for top nav across all screens

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -1,21 +1,31 @@
 window.onresize = function() {displayFunction()};
 
 function displayFunction() {
-  if (window.innerWidth < 768) {
+  if (window.innerWidth < 992) {
     document.getElementById("first-navbar").style.top = "0";
   } else {
-    document.getElementById("first-navbar").style.top = "-60px";
-  }
+      document.getElementById("first-navbar").style.top = "-60px";
+    }
 }
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementById("first-navbar").style.top = "0";
-    document.getElementById("nav-hide").style.display = "block";
-  } else {
-    document.getElementById("first-navbar").style.top = "-60px";
-    document.getElementById("nav-hide").style.display = "none";
+  if (window.innerWidth >= 992) {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+      document.getElementById("first-navbar").style.top = "0";
+      document.getElementById("nav-hide").style.display = "block";
+    } else {
+        document.getElementById("first-navbar").style.top = "-60px";
+        document.getElementById("nav-hide").style.display = "none";
+    }
+  } else if (window.innerWidth < 992) {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+      document.getElementById("first-navbar").style.top = "0";
+      document.getElementById("nav-hide").style.display = "block";
+    } else {
+        document.getElementById("first-navbar").style.top = "0";
+        document.getElementById("nav-hide").style.display = "block";
+    }
   }
 }
